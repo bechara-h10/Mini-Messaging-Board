21d24c73a8ae174097fdcc85dc3c476a789f3615
bechara-h10 <bechara.hosri1@gmail.com>
2024-02-09 14:39:36 +0200

    Adding netlify functions

--- functions/app.js
+++ functions/app.js
@@ -5,8 +5,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const serverless = require("serverless-http");
 
-const indexRouter = require("../routes/index");
-const usersRouter = require("../routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
